f8d0336a39da6139b9e25a051dcb837e2f496501
Katasravani <105917453+Katasravani@users.noreply.github.com>
2024-06-17 15:31:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,20 +20,41 @@ function firstWord(str) {
 	// return trip.substring(0,result);
 
 
-	let trip=str.trim();
-	for (let i = 0; i <trip.length; i++) {
-		if (trip[i]!==' ') {
-			let result=trip.indexOf(' ');
-			return trip.substring(0,result);
-		}
-	}
-	return ' ';
+	// let trip=str.trim();
+	// for (let i = 0; i <trip.length; i++) {
+	// 	let result=trip.indexOf(' ');
+	// 	if (trip[i]!==' ') {
+	// 		return trip.substring(0,result);
+	// 	}
+	// }
+	// return ' ';
+
+
+	 str=str.trim();
+	 if (str.length === 0) {
+        return '';
+    }
+    
+    // Find the index of the first space
+    let firstSpaceIndex = str.indexOf(' ');
+
+    // If there's no space in the string, return the entire string
+    if (firstSpaceIndex === -1) {
+        return str;
+    }
+
+    // Return the substring from the start to the first space
+    return str.substring(0, firstSpaceIndex);
+	
 }
 
 // Taking user input in a browser environment
 const userInput = prompt('Enter a string:'); // This will prompt the user to enter a string
 const result = firstWord(userInput);
 alert(result);
+
+
+
 
 
 
