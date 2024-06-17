8992dcd20c739ea296603348497ebf5d1027eace
Katasravani <105917453+Katasravani@users.noreply.github.com>
2024-06-17 12:58:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 function firstWord(str) {
-    if (str === '' || !str.includes(' ')) {
-        return str;
-    }
+    // if (str === '') {
+    //     return str;
+    // }
 
-    const spaceIndex = str.indexOf(' ');
-    return str.substring(0, spaceIndex);
+    // const spaceIndex = str.indexOf(' ');
+    // return str.substring(0, spaceIndex);
+
+
+	let result=str.trim();
+	for (let i = 0; i < result.length; i++) {
+		// let spaceIndex=result.indexOf(' ');
+		if (result[i]!==' ') {
+			let spaceIndex=result.indexOf(' ');
+			return result.substring(0,spaceIndex);
+		}
+	}
+	return '';
 }
 
 // Taking user input in a browser environment
 const userInput = prompt('Enter a string:'); // This will prompt the user to enter a string
 const result = firstWord(userInput);
 alert(result);
+
+
+
+
 
 
