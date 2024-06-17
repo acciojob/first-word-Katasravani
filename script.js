835337d494d5e8249219febde3097caae6e37ae3
Katasravani <105917453+Katasravani@users.noreply.github.com>
2024-06-17 15:20:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,21 +7,36 @@ function firstWord(str) {
     // return str.substring(0, spaceIndex);
 
 
-	let result=str.trim();
-	for (let i = 0; i < result.length; i++) {
-		// let spaceIndex=result.indexOf(' ');
-		if (result[i]!==' ') {
-			let spaceIndex=result.indexOf(' ');
-			return result.substring(0,spaceIndex);
+	// let trip=str.trim();
+	
+	// if (str.length==0) {
+	// 	return '';
+	// }
+	// let result=str.indexOf(' ');
+	// if (result==-1) {
+	// 	return str;
+	// }
+	
+	// return trip.substring(0,result);
+
+
+	let trip=str.trim();
+	for (let i = 0; i <trip.length; i++) {
+		if (trip[i]!==' ') {
+			let result=trip.indexOf(' ');
+			return trip.substring(0,result);
 		}
 	}
-	return '';
+	return ' ';
 }
 
 // Taking user input in a browser environment
 const userInput = prompt('Enter a string:'); // This will prompt the user to enter a string
 const result = firstWord(userInput);
 alert(result);
+
+
+
 
 
 
